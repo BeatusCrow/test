b969a393a8dafd331a5643f4ee3daeefdd128530
BeatusCrow <ivan.poluhtin.06@bk.ru>
2024-01-02 22:04:31 +0300

    02.01.24 (3)
    add jre to bat and vbs files

--- the program to run/testjar.bat
+++ the program to run/testjar.bat
@@ -1,1 +1,1 @@
-java --module-path "%~dp0\javafx-sdk-17.0.9\lib" --add-modules javafx.controls,javafx.fxml -Dfile.encoding=UTF-8 -jar "%~dp0\other\test.jar"
+"%~dp0\jdk-21\bin\java.exe" --module-path "%~dp0\javafx-sdk-17.0.9\lib" --add-modules javafx.controls,javafx.fxml -Dfile.encoding=UTF-8 -jar "%~dp0\other\test.jar"
